17d6731f27aa9cf8ab0efe27ff640e230b70a4cf
Dk314053 <dk314053@gmail.com>
2023-06-07 22:11:28 +0530

    pahada kisi bhi number ka

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import { useState } from 'react';
 
 function App() {
+const [update,setUpdate]=useState("")
+const [data,setData]=useState([1,2,3,4,5,6,7,8,9,10])
+
+const handleChange=(e)=>{
+setUpdate(e.target.value)
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <h1 className='head'>Print Table</h1>
+     <input className='input' type='text' placeholder='type any number' value={update} onChange={handleChange} />
+     <ul>
+     {
+   update?(data.map((ele,index)=>{
+     return(
+     <li style={{listStyleType:"none",marginRight:"1.8rem",fontSize:"1.5rem"}} key={index}>{ele*update}</li>
+     )})):" "
+     }
+     </ul>
     </div>
   );
 }
